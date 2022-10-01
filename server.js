37eb79d252ce1b99e9325fa09d449387f2027f6f
Paul Butler <paul@driftingin.space>
2022-10-01 15:15:53 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const HOSTNAME = '0.0.0.0';
+const PORT = parseInt(process.env.PORT || 8080);
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  console.log(`Returning result to ${req.socket.remoteAddress}`)
+
+  res.end(req.socket.remoteAddress);
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
