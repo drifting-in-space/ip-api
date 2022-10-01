aadafe800cbc8f0e1d470abc51c698b90037f1aa
Paul Butler <paul@driftingin.space>
2022-10-01 16:09:06 -0400

    handle signals

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const HOSTNAME = '0.0.0.0';
 const PORT = parseInt(process.env.PORT || 8080);
 
+process.on('SIGINT', process.exit);
+process.on('SIGTERM', process.exit);
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   console.log(`Returning result to ${req.socket.remoteAddress}`)
